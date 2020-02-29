60ba7f9c0b9d94ee4f46db8ad370a5e2338187c0
Siddhant Jain <sid@Siddhants-MacBook-Pro.local>
2020-02-29 13:50:09 +0530

    Adding Dashboard componet

---
+++ ppmtool-react-client/src/components/Dashboard/Dashborad.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashborad extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to PPM</h1>
+            </div>
+        );
+    }
+}
+
+export default Dashborad;
