fa7677f23044c13864d3eccf5000971657fd76fe
Siddhant Jain <sid@Siddhants-MacBook-Pro.local>
2020-03-01 11:24:45 +0530

    Adding Reducer

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,19 +3,22 @@ import './App.css';
 import Dashborad from './components/Dashboard/Dashborad';
 import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import AddProject from './Project/AddProject/AddProject';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route exact path = "/dashboard" component = {Dashborad}/>
-        <Route exact path = "/addProject" component = {AddProject}/>
-      </div>
-    </Router>
-
+    <Provider store = {store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashborad} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
